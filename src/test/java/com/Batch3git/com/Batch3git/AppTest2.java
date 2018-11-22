a59fd41925c7db9f59e6c683f78d9376fbb1b751
msamuel9 <msamuel9@LH7U05CG51708SQ.ms.ds.uhc.com>
2018-11-22 08:11:49 +0530

    Added Apptest2(by Sam)

---
+++ src/test/java/com/Batch3git/com/Batch3git/AppTest2.java
@@ -0,0 +1,13 @@
+package com.Batch3git.com.Batch3git;
+
+import org.testng.annotations.Test;
+
+public class AppTest2 {
+	
+	@Test
+	public void test1fromApptest()
+	{
+		System.out.println("This is apptest2");
+	}
+
+}
