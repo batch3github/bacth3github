c5dd5201c007fd348d2d5c87576ac24ef080f594
msamuel9 <msamuel9@LH7U05CG51708SQ.ms.ds.uhc.com>
2018-11-22 08:01:28 +0530

    Initial Commit (11/22)

---
+++ src/test/java/com/Batch3git/com/Batch3git/AppTest.java
@@ -0,0 +1,30 @@
+package com.Batch3git.com.Batch3git;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    @Test
+    public void Test1()
+    {
+    	System.out.println("This is test 1 ");
+    }
+  
+    @Test
+    public void Test2()
+    {
+    	System.out.println("This is test 2 ");
+    }
+    
+    @Test
+    public void Test3()
+    {
+    	System.out.println("This is test 3 ");
+    }
+ 
+}
